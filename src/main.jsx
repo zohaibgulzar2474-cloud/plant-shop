c82aed20a44b6c62d904d3f1f613718865fce8d3
Zohaib Gulzar <zohaibgulzar2474@gmail.com>
2025-08-27 01:44:29 +0500

    a

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./store.js";
-import "./styles.css";
+import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
